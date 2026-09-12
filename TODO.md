99a611e3c8bb0121ab5640dda48a16f642beb8ae
MarlicVonBarien <johnsnat2026@proton.me>
2026-09-12 18:18:19 -0400

    Added basic roadmap

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO List and Features
+ - [ ] Create a graphical TUI display
+ - [ ] Add buttons for start/stop/reset
+ - [ ] Take an input for the amount of time.
+ - [ ] Play a notification sound 
+ - [ ] Push a notification to the screen with the notification daemon.
---
+++ TUITimer.py
@@ -0,0 +1,9 @@
+# ======================================================================
+# TODO List and Features
+# ======================================================================
+# TODO: Create a graphical TUI display
+# TODO: Add buttons for start/stop/reset
+# TODO: Take an input for the amount of time.
+# TODO: Play a notification sound 
+# TODO: Push a notification to the screen with the notification daemon.
+# ======================================================================
